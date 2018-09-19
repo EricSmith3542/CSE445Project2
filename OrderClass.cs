ddea8b58685abcc6fd7c9cb80c89ff9cca035f8d
dayakulkarniasu <35940519+dayakulkarniasu@users.noreply.github.com>
2018-09-19 12:56:48 -0700

    OrderClass

---
+++ OrderClass.cs
@@ -0,0 +1,62 @@
+#include "orderclass.hpp"
+using System;
+
+namespace Assignment_2
+{
+    
+    public class OrderClass
+    {
+        //private vars
+    /*
+     - senderId: the identity of the sender, you can use thread name or thread id.
+     - cardNo: an integer that represents a credit card number.
+     - receiverID: the identity of the receiver, you can use thread name or a unique name defined for a publisher.
+     - amount: an integer that represents the number of books to order.
+     - unit price: a double that represents the unit price of the book received from the publisher.
+     */
+        private string senderId;
+        private Int32 cardNo;
+        private string revieverID;
+        private Int32 amount;
+        private double unitPrice,
+        
+        public OrderClass(string senderId, Int32 cardNo, Int32 amount) //constructor
+        {
+            this.senderId = senderId;
+            
+            this.cardNo = cardNo;
+            
+            this.reviewerID = reviewerID;
+            
+            this.amount = amount;
+            
+            this.unitPrice = unitPrice;
+            
+        }
+        //getters
+  
+        public string getSenderId()
+        {
+            return senderId;
+        }
+        public Int32 getCardNo()
+        {
+            return cardNo;
+        }
+        
+        public string getReviewerID()
+        {
+            return reviewerID;
+        }
+        public Int32 getAmount)
+        {
+            return amount;
+        }
+        public double getUnitPrice()
+        {
+            return uniteprice;
+        }
+        
+        
+    }
+}
